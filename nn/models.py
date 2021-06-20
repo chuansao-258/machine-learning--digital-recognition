81d8856fd1b396160981e7d6a34df0776d96d0d5
chuansao-258 <71584623+chuansao-258@users.noreply.github.com>
2021-06-20 14:46:36 +0800

    Update models.py

--- nn/models.py
+++ nn/models.py
@@ -1,67 +1,23 @@
 import nn
 import time
-
-
 class DigitClassificationModel(object):
-    """
-    Each handwritten digit is a 28x28 pixel grayscale image, which is flattened
-    into a 784-dimensional vector for the purposes of this model. Each entry in
-    the vector is a floating point number between 0 and 1.
-
-    """
     def __init__(self):
-        # Initialize your model parameters here
-        "*** YOUR CODE HERE ***"
         self.batch_size = 10
-        self.m1 = nn.Parameter(784,100)
-        self.m2 = nn.Parameter(100,10)
-        self.b1 = nn.Parameter(1,100)
+        self.m1 = nn.Parameter(784,15)
+        self.m2 = nn.Parameter(15,10)
+        self.b1 = nn.Parameter(1,15)
         self.b2 = nn.Parameter(1,10)
-
     def run(self, x):
-        """
-        Runs the model for a batch of examples.
-
-        Your model should predict a node with shape (batch_size x 10),
-        containing scores. Higher scores correspond to greater probability of
-        the image belonging to a particular class.
-
-        Inputs:
-            x: a node with shape (batch_size x 784)
-        Output:
-            A node with shape (batch_size x 10) containing predicted scores
-                (also called logits)
-        """
-        "*** YOUR CODE HERE ***"
         xm = nn.Linear(x,self.m1)
         a1 = nn.AddBias(xm, self.b1)
         a2 = nn.ReLU(a1)
         a3 = nn.Linear(a2,self.m2)
         a4 = nn.AddBias(a3, self.b2)
         return a4
-
     def get_loss(self, x, y):
-        """
-        Computes the loss for a batch of examples.
-
-        The correct labels `y` are represented as a node with shape
-        (batch_size x 10). Each row is a one-hot vector encoding the correct
-        digit class (0-9).
-
-        Inputs:
-            x: a node with shape (batch_size x 784)
-            y: a node with shape (batch_size x 10)
-        Returns: a loss node
-        """
-        "*** YOUR CODE HERE ***"
         answer = self.run(x)
         return nn.SoftmaxLoss(answer, y)
-
     def train(self, dataset):
-        """
-        Trains the model.
-        """
-        "*** YOUR CODE HERE ***"
         multiplier=-0.04
         for i in range(10000):
             sample = 0
@@ -70,7 +26,6 @@ class DigitClassificationModel(object):
                 loss = self.get_loss(x,y)
                 #print(nn.as_scalar(loss))
                 grad_wrt_m1,grad_wrt_b1,grad_wrt_m2,grad_wrt_b2=nn.gradients(loss,[self.m1,self.b1,self.m2,self.b2])
-                
                 self.m1.update(grad_wrt_m1, multiplier)
                 self.m2.update(grad_wrt_m2, multiplier)
                 self.b1.update(grad_wrt_b1, multiplier)
